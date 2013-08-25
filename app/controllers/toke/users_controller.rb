5c38680dd4b3e35b9c10c0b043e6357cc3215bb0
Jack Flannery <jmflannery81@gmail.com>
2013-08-24 22:02:57 -0400

    start to implement users controller

--- app/controllers/toke/users_controller.rb
+++ app/controllers/toke/users_controller.rb
@@ -1,10 +1,16 @@
 require_dependency "toke/application_controller"
 
 module Toke
+
   class UsersController < ApplicationController
 
     def create
-      render text: 'Toke'
+      user = User.new(person_params)
+      head user.valid? ? 201 : 500
+    end
+
+    def person_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
     end
   end
 end
--- test/controllers/toke/users_controller_test.rb
+++ test/controllers/toke/users_controller_test.rb
@@ -1,12 +1,29 @@
 require 'test_helper'
 
 module Toke
+
   describe UsersController do
 
     describe "POST create" do
-      it "the truth" do
-        post :create, use_route: 'toke'
-        response.body.must_equal "Toke"
+
+      describe "given valid atributes" do
+
+        let(:user_attrs) {{ username: 'ichiro', password: 'suzuki', password_confirmation: 'suzuki' }}
+
+        it "it responds with 201" do
+          post :create, user: user_attrs, use_route: 'toke'
+          response.status.must_equal 201
+        end
+      end
+
+      describe "given invalid atributes" do
+
+        let(:user_attrs) {{ username: 'ichiro', password: 'suzuki', password_confirmation: 'matsui' }}
+
+        it "it responds with 500" do
+          post :create, user: user_attrs, use_route: 'toke'
+          response.status.must_equal 500
+        end
       end
     end
   end
