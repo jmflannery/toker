23a31115a15472493e38ce7945539708facdb87e
Jack Flannery <jmflannery81@gmail.com>
2013-09-05 21:21:20 -0400

    put User integration test in Toke module

--- spec/features/toke/user_spec.rb
+++ spec/features/toke/user_spec.rb
@@ -2,34 +2,37 @@ require 'spec_helper'
 
 OUTER_APP = Rack::Builder.parse_file(File.expand_path('../../../dummy/config.ru', __FILE__)).first
 
-describe "User" do
-  include Rack::Test::Methods
+module Toke
 
-  let(:app) { OUTER_APP }
+  describe "User" do
+    include Rack::Test::Methods
 
-  describe 'Create' do
+    let(:app) { OUTER_APP }
 
-    let(:user_attrs) {{ username: "jack", password: "secret", password_confirmation: "secret" }}
+    describe 'Create' do
 
-    describe 'with valid user attributes' do
+      let(:user_attrs) {{ username: "jack", password: "secret", password_confirmation: "secret" }}
 
-      it 'returns the user as JSON with 201 status' do
-        post "/toke/users", user: user_attrs
-        expect(last_response.status).to eq 201
-        expect(last_response.body).to match /^{"user":{"id":\d+,"username":"jack"}}$/
+      describe 'with valid user attributes' do
+
+        it 'returns the user as JSON with 201 status' do
+          post "/toke/users", user: user_attrs
+          expect(last_response.status).to eq 201
+          expect(last_response.body).to match /^{"user":{"id":\d+,"username":"jack"}}$/
+        end
       end
     end
-  end
 
-  describe 'Index' do
-    
-    let!(:user1) { Toke::User.create(username: 'mark', password: 'secret', password_confirmation: 'secret') }
-    let!(:user2) { Toke::User.create(username: 'anthony', password: 'secret', password_confirmation: 'secret') }
+    describe 'Index' do
+      
+      let!(:user1) { User.create(username: 'mark', password: 'secret', password_confirmation: 'secret') }
+      let!(:user2) { User.create(username: 'anthony', password: 'secret', password_confirmation: 'secret') }
 
-    it "gets all users as JSON with 200 status" do
-      get "/toke/users"
-      expect(last_response.status).to eq 200
-      expect(last_response.body).to match /^{"users":\[{"id":\d+,"username":"mark"},{"id":\d+,"username":"anthony"}\]}/
+      it "gets all users as JSON with 200 status" do
+        get "/toke/users"
+        expect(last_response.status).to eq 200
+        expect(last_response.body).to match /^{"users":\[{"id":\d+,"username":"mark"},{"id":\d+,"username":"anthony"}\]}/
+      end
     end
   end
 end
