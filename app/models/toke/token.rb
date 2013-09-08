3322d00fc97e22f10b6845180bdbc63dfd7eb1dc
Jack Flannery <jmflannery81@gmail.com>
2013-09-08 13:02:58 -0400

    modify Token to not initially be expired and add toke method to generate token

--- app/models/toke/token.rb
+++ app/models/toke/token.rb
@@ -1,16 +1,27 @@
 module Toke
   class Token < ActiveRecord::Base
     belongs_to :user
-    before_create :generate_token, :set_expiry
+    before_create :expire
+
+    def toke
+      generate_token
+      set_expiry
+      save
+    end
 
     private
+
+    def expire
+      self.expires_at = Time.now
+      self.key = nil
+    end
 
     def generate_token
       self.key = SecureRandom.hex
     end
 
-    def set_expiry
-      self.expires_at = 4.hours.from_now
+    def set_expiry(expiration = 4.hours.from_now)
+      self.expires_at = expiration
     end
   end
 end
--- spec/models/toke/token_spec.rb
+++ spec/models/toke/token_spec.rb
@@ -6,17 +6,26 @@ module Toke
     
     let(:user) { FactoryGirl.create(:user) }
     subject { Token.new(user: user) }
+    before { subject.save }
+
+    it "initially has a blank key" do
+      expect(subject.key).to be_blank
+    end
+
+    it "is initially expired" do
+      expect(subject.expires_at).to be < Time.now
+    end
 
     it "sets the token key" do
-      subject.save
-      expect(subject.key).to match /\S{32}/
+      subject.toke
+      expect(subject.reload.key).to match /\S{32}/
     end
 
     it "sets the token expiration" do
       now = Time.now
       Time.stub(:now).and_return(now)
-      subject.save
-      expect(subject.expires_at).to eq (now + 4.hours)
+      subject.toke
+      expect(subject.reload.expires_at).to eq (now + 4.hours).to_formatted_s(:rfc822)
     end
   end
 end
