e726ba1f77761e8b056f33624236af52463d91d6
Jack Flannery <jmflannery81@gmail.com>
2013-09-07 22:27:52 -0400

    session integration test

---
+++ spec/features/toke/session_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+OUTER_APP = Rack::Builder.parse_file(File.expand_path('../../../dummy/config.ru', __FILE__)).first
+
+describe "Sessions" do
+
+  include Rack::Test::Methods
+
+  let(:app) { OUTER_APP }
+
+  describe "create Session" do
+
+    context "given valid parameters" do
+
+      let(:user) { Toke::User.create(username: 'niki', password: 'secret', password_confirmation: 'secret') }
+      let(:sesh_attrs) {{ username: 'nikki', password: 'secret' }}
+
+      it "returns an token that expires in 4 hours with 201 status" do
+        now = Time.now
+        Time.stub(:now).and_return(now)
+        post 'toke/sessions', sesh_attrs
+        expect(response.status).to be 201
+        expect(response.body).to match /^{"token":{"token":"\S{32}","expires_at":#{(now + 4.hours)}}}$/
+      end
+    end
+  end
+end
