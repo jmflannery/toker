0b8114bed42419e963c1895a451d37c8baf2974b
Jack Flannery <jmflannery81@gmail.com>
2013-09-08 11:41:34 -0400

    add some user validations

--- app/models/toke/user.rb
+++ app/models/toke/user.rb
@@ -1,5 +1,8 @@
 module Toke
   class User < ActiveRecord::Base
     has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, length: { in: 6..50 }
   end
 end
--- spec/models/toke/user_spec.rb
+++ spec/models/toke/user_spec.rb
@@ -4,12 +4,45 @@ module Toke
 
   describe User do
 
-    let(:valid_params) {{ username: 'ichiro', password: 'suzuki', password_confirmation: 'suzuki' }}
+    let(:user_attrs) { FactoryGirl.attributes_for(:user) }
+    subject { User.new(user_attrs) }
 
-    subject { User.new(valid_params) }
+    it "has a valid factory" do
+      expect(FactoryGirl.build(:user)).to be_valid
+    end
 
-    it "is valid with valid params" do
+    it "is valid with valid attributes" do
       expect(subject).to be_valid
+    end
+
+    it "is invalid without a username" do
+      expect(FactoryGirl.build(:user, username: nil)).to have(1).errors_on(:username)
+    end
+
+    it "is invalid with a duplicate username" do
+      FactoryGirl.create(:user, username: 'fred')
+      expect(FactoryGirl.build(:user, username: 'fred')).to have(1).errors_on(:username)
+    end
+
+    it "is invalid without a password" do
+      expect(FactoryGirl.build(:user, password: nil)).to have(2).errors_on(:password)
+    end
+
+    it "is invalid without a password_confirmation" do
+      expect(FactoryGirl.build(:user, password_confirmation: nil)).to have(1).errors_on(:password_confirmation)
+    end
+
+    it "is invalid if password_confirmation doesn't match password" do
+      expect(FactoryGirl.build(:user, password_confirmation: "wrong")).to have(1).errors_on(:password_confirmation)
+    end
+
+    it "is invalid with a short password under 6 chars" do
+      expect(FactoryGirl.build(:user, password: "lesss", password_confirmation: "lesss")).to have(1).errors_on(:password)
+    end
+
+    it "is invalid with a long password over 50 chars" do
+      tolong = 's' * 51
+      expect(FactoryGirl.build(:user, password: tolong, password_confirmation: tolong)).to have(1).errors_on(:password)
     end
   end
 end
