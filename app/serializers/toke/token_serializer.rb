c397bd83b39ffe188ad19c3868566edb8604548e
Jack Flannery <jmflannery81@gmail.com>
2013-12-08 16:11:18 -0500

    add the user_id to the serialized token

--- app/serializers/toke/token_serializer.rb
+++ app/serializers/toke/token_serializer.rb
@@ -1,6 +1,6 @@
 module Toke
   class TokenSerializer < ActiveModel::Serializer
-    attributes :id, :key, :expires_at
+    attributes :id, :key, :expires_at, :user_id
 
     def expires_at
       object.expires_at.to_s(:db)
