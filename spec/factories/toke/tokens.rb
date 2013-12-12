280c1ceccfc8e3440497c090c086be2370d6413e
Jack Flannery <jmflannery81@gmail.com>
2013-12-12 18:24:27 -0500

    remove body from factory because the model callbacks populate the key and expires_at

--- spec/factories/toke/tokens.rb
+++ spec/factories/toke/tokens.rb
@@ -1,8 +1,4 @@
 FactoryGirl.define do
   factory :token, class: Toke::Token do |token|
-    sequence(:key) do |n|
-      'x' * (32 - n.to_s.length) + n.to_s
-    end
-    expires_at Time.now + 4.hours
   end
 end
