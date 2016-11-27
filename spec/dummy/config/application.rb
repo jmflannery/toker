3c6994d6b01c273557440dbbc7142361898015ff
Jack Flannery <jmflannery81@gmail.com>
2016-11-27 11:26:01 -0500

    Configure spec/dummy app to be API only

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,5 +13,6 @@ module Dummy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.api_only = true
   end
 end
