939e2a84df879778f93e2eb2bc77e28064cd6288
Jack Flannery <jmflannery81@gmail.com>
2017-03-16 22:43:22 -0400

    Include module in application controller

--- app/controllers/toker/application_controller.rb
+++ app/controllers/toker/application_controller.rb
@@ -1,6 +1,7 @@
 module Toker
   class ApplicationController < ActionController::API
     include ActionController::HttpAuthentication::Basic::ControllerMethods
+    include ActionController::HttpAuthentication::Basic::ControllerMethods::ClassMethods
     include Toker::TokenAuthentication
   end
 end
